94f768793d74ff8cba3a95e8c9ef11a1a97c2cc9
IljaTrunov <93324206+IljaTrunov@users.noreply.github.com>
2022-01-19 14:04:25 +0200

    Update slovarik.py

--- slovarik.py
+++ slovarik.py
@@ -1,20 +1,20 @@
 from module1 import *
-rus=[]
-eng=[]
-variant=""
-print("Привет!")
-variant=int(input("Ты зашел в словарь. Напиши что ты хочешь сделать: \n1-перевести слово с русского на английский из предоставленных \n2-добавить свое слово и свой перевод \n3-перевести слово с английского на русский из предоставленных \n4-выйти \nВводи: "))
-def choice():
-    if variant==1:
-        RuEng()
-    elif variant==2:
-        AddWordTranslate()
-    elif variant==3:
-        EngRu()
-    elif variant==4:
-        quit
-def RuEng():
-    fail=open(russkiy.txt,"r")
-    fail=open(english.txt,"r")
-    slovo=input("Выбери слово, которое хочешь перевести: ")
-    if slovo=="арбуз":
+rus_list=[]
+eng_list=[]
+rus_list=failist_lugemine("russkiy.txt",rus_list)
+eng_list=failist_lugemine("english.txt",eng_list)
+while True:
+    print("Привет, ты зашел в словарь русско-английского и английско-русского перевода! \nЧто ты хочешь сделать?")
+    menu=input("\ntranslate - перевести слово \nadd - добавить слово \nall - все слова, которые есть \ncontrol - Произвести слово \nerror - исправить слово \nВводи: ")
+    if menu=="t":
+        translate()
+    elif menu=="add":
+        rus_list=uus_sona("russkiy.txt",input("Новое слово:"))
+        eng_list=uus_sona("english.txt",input("Uus sõna:"))
+    elif menu=="all":
+        print(rus_list)
+        print(eng_list)
+    elif menu=="control":
+         wordControl()
+    elif menu=="error":
+        viga()
