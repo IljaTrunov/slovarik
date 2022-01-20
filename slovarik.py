da14447f23b1e12742af04f4b1cfa7c16cff3dbb
IljaTrunov <93324206+IljaTrunov@users.noreply.github.com>
2022-01-20 18:08:10 +0200

    Update slovarik.py

--- slovarik.py
+++ slovarik.py
@@ -5,7 +5,7 @@ rus_list=failist_lugemine("russkiy.txt",rus_list)
 eng_list=failist_lugemine("english.txt",eng_list)
 while True:
     print("Привет, ты зашел в словарь русско-английского и английско-русского перевода! \nЧто ты хочешь сделать?")
-    menu=input("\ntranslate - перевести слово \nadd - добавить слово \nall - все слова, которые есть \ncontrol - Произвести слово \nerror - исправить слово \nВводи: ")
+    menu=input("\ntranslate - перевести слово \nadd - добавить слово \nall - все слова, которые есть \ncontrol - Произвести слово \nerror - ошибка \ncorrection - исправить \nВводи: ")
     if menu=="t":
         translate(rus_list,eng_list)
     elif menu=="add":
@@ -14,8 +14,8 @@ while True:
     elif menu=="all":
         print(rus_list)
         print(eng_list)
-    elif menu=="control":
-         wordControl()
     elif menu=="error":
         viga()
+    elif menu=="correction"
+        correction()
 
